74d279987c808cc679732ccba3cdbf4c5ff70068
gawelszymon <szymon.gawel.szg@gmail.com>
2024-10-20 19:32:16 +0200

    test

---
+++ backend/data_loader.py
@@ -0,0 +1,17 @@
+import csv
+
+import pandas as pd
+
+# with open('cennik.csv', newline='') as tariff:
+#     tariffreader =  csv.reader(tariff, delimiter=';')
+#     rows = list(tariffreader)
+    
+#     for i in range(2):
+#         print(' | \n'.join(rows[i]))
+#         print("_____________________________________________________")
+
+
+tariff_file = 'cennik.csv'
+df = pd.read_csv(tariff_file, delimiter=';')
+
+print(df.head(10))
