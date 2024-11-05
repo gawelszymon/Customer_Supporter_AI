20f556a53df1dbe0a9355a02034dd801852c7b48
gawelszymon <szymon.gawel.szg@gmail.com>
2024-11-06 00:06:49 +0100

    csv error solved

--- backend/csv_analizer.py
+++ backend/csv_analizer.py
@@ -1,19 +1,7 @@
-import re
+with open("base/cennik.csv", 'r') as file:
+    content = file.read()
 
-import pandas as pd
+content = content.replace('`', ';')
 
-
-def bad_field_dropper():
-    df = pd.read_csv("base/cennik.csv", sep=";")
-
-    try:
-        df_e = pd.read_csv("base/cennik.csv")
-    except pd.errors.ParserError as e:
-        error_msg = str(e)
-        match = re.search(r'line (\d+)', error_msg)
-        idx = int(match.group(1))
-        df = df.drop(idx - 2)
-        df.to_csv("base/cennik.csv", sep=";")
-        bad_field_dropper()
-        
-bad_field_dropper()
+with open("base/cennik.csv", 'w') as file:
+    file.write(content)
