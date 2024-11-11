38e016aadb7fb69344c1b9477b0f60bcaa12aecb
gawelszymon <szymon.gawel.szg@gmail.com>
2024-11-11 23:56:47 +0100

    model developing

--- backend/beta_language_model.py
+++ backend/beta_language_model.py
@@ -2,8 +2,9 @@ import os
 
 import pandas as pd
 from llama_index.core import SimpleDirectoryReader
+from llama_index.core.node_parser import SentenceSplitter
 
-base_folder = "./base"
+base_folder = "./base1"
 
 file_path = "./base/cennik.csv"
 
@@ -15,3 +16,11 @@ docs = reader.load_data()
 
 for doc in docs:
     print(doc.get_metadata_str())
+    
+# docs[0].__dict__      #by that command you can get access to interior directory of object docs[0]
+
+node_parser = SentenceSplitter(chunk_size= 200, chunk_overlap=0) # it parts the data for the chunks that are used to the nodes
+
+nodes = node_parser.get_nodes_from_documents(docs, show_progress=True) # that is the assignment of the document to the nodes
+
+print(len(nodes))
